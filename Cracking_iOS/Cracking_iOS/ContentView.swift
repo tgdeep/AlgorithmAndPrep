c19f249bde819d4125991d0f53f4e1fc30966867
DEEPAK T G <deepak.tsg@gmail.com>
2023-12-23 22:55:00 -0500

    Updates to the xcode file

---
+++ Cracking_iOS/Cracking_iOS/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Cracking_iOS
+//
+//  Created by DEEPAK T G on 12/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Deepak T G")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cracking_iOS/Cracking_iOS/Cracking_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cracking_iOSApp.swift
+//  Cracking_iOS
+//
+//  Created by DEEPAK T G on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Cracking_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
